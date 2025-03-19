932fa7b8ae8ec7c89c30480a127c8c5c5315741c
Srrdrr <anvarovsardor926@gmail.com>
2025-03-19 21:40:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let  get = prompt('ismingizni kiriting') 
+
+let rer = prompt('Bironta harf ayting')
+
+alert(`Sardor ismining  R harifi mavjud`)
+
+let  getq = prompt('ismingizni kiriting') 
+
+let rert = prompt('Bironta harf ayting')
+
+alert(`Sardor ismining t harifi mavjud emas`)
